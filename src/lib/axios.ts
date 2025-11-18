69fb839d84f2d377aa9357d00224309611829dae
mrHuyCoi <huytran998811@gmail.com>
2025-11-18 21:27:03 +0700

    Update axios.ts

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import qs from 'qs';
 
 const apiClient = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/v1',
+  baseURL: import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/api/v1',
   timeout: 10000,
 });
 
