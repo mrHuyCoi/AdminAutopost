ffc3c04e11efb195a38a876370e5eb819d8027ef
mrHuyCoi <huytran998811@gmail.com>
2025-11-22 12:27:03 +0700

    Update axios.ts

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -6,7 +6,7 @@ const TOKEN_KEY = 'accessToken';
 
 const apiClient = axios.create({
   // baseURL: 'http://127.0.0.1:8000/api/v1',
-  baseURL: 'https://afeea541b3ca.ngrok-free.app/api/v1',
+  baseURL: 'https://ed686fcf75e0.ngrok-free.app/api/v1',
   timeout: 10000,
 });
 
